54be166de94ae1821e500657d02b121caa22009b
Pawin <pawin.taechoyotin@gmail.com>
2022-12-06 17:41:04 -0700

    Encode labels to vector space with dim of 10

--- helper.py
+++ helper.py
@@ -1,40 +1,56 @@
-# from typing import 
+# from typing import
 import pandas as pd
 # Load Data from files
 # Load arguments
-def load_arguments(filename:str):
+
+
+def load_arguments(filename: str):
     with open(filename, "r", encoding="utf8") as f:
         lines = f.readlines()
         headers = lines[0].strip().split("\t")
         records = [l.strip().split("\t") for l in lines[1:]]
     return headers, records
 
-def load_arguments_as_df(filename:str):
+
+def load_arguments_as_df(filename: str):
     return pd.read_csv(filename, encoding="utf8", sep="\t").set_index("Argument ID")
 
-def load_value_categories(filename:str):
+
+def load_value_categories(filename: str):
     return pd.read_csv(filename, encoding="utf8", sep="\t").set_index("Argument ID")
 # Encode labels to vector space
 
 opposite_pairs = {
-    "0": [],
-    "1": [],
-    "2": [],
-    "3": [],
-    "4": [],
-    "5": [],
-    "6": [],
-    "7": [],
-    "8": [],
-    "9": [],
-    "10": [],
-    "11": [],
-    "12": [],
-    "13": [],
-    "14": [],
-    "15": [],
-    "16": [],
-    "17": [],
-    "18": [],
-    "19": [],
+    "Self-direction: thought": (0, 1),
+    "Self-direction: action": (1, 1),
+    "Stimulation": (2, 1),
+    "Hedonism": (3, 1),
+    "Achievement": (4, 1),
+    "Power: dominance": (5, 1),
+    "Power: resources": (6, 1),
+    "Face": (7, 1),
+    "Security: personal": (8, 1),
+    "Security: societal": (9, 1),
+    "Tradition": (0, -1),
+    "Conformity: rules": (1, -1),
+    "Conformity: interpersonal": (2, -1),
+    "Humility": (3, -1),
+    "Benevolence: caring": (4, -1),
+    "Benevolence: dependability": (5, -1),
+    "Universalism: concern": (6, -1),
+    "Universalism: nature": (7, -1),
+    "Universalism: tolerance": (8, -1),
+    "Universalism: objectivity": (9, -1),
 }
+
+pair_dict = {0: ['Self-direction: thought', 'Tradition'], 1: ['Self-direction: action', 'Conformity: rules'], 2: ['Stimulation', 'Conformity: interpersonal'], 3: ['Hedonism', 'Humility'], 4: ['Achievement', 'Benevolence: caring'],
+             5: ['Power: dominance', 'Benevolence: dependability'], 6: ['Power: resources', 'Universalism: concern'], 7: ['Face', 'Universalism: nature'], 8: ['Security: personal', 'Universalism: tolerance'], 9: ['Security: societal', 'Universalism: objectivity']}
+
+
+def encode_label(inputRow):
+    output_vector = [0] * len(pair_dict)
+    for k, v in pair_dict.items():
+        output_vector[k] = inputRow[v[0]] + inputRow[v[1]] * -1
+    return output_vector
+
+
